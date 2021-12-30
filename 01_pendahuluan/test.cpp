252b3a3522ff6d283adf231ba23f1e3a0207ce95
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2021-12-30 17:11:38 +0700

    RECODE Update test.cpp

--- 01_pendahuluan/test.cpp
+++ 01_pendahuluan/test.cpp
@@ -14,3 +14,43 @@ int main()
 	std::cout << a << std::endl;
 	return 0;
 }
+
+
+
+//
+//==========RECODE()==========
+//
+
+
+
+#include <iostream>
+//comment
+/*
+multiline comment
+*/
+
+int main() //main door for enter the house, the house is synonym of this program
+{
+    int a;
+    std::cout << "hi" << "its me" << std::endl;
+    std::cin >> a;
+    std::cout << a << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
