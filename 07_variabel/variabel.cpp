6180697b311dc1ceb980c23980e33f7ae6f62e90
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-03 16:00:18 +0700

    Update variabel.cpp

--- 07_variabel/variabel.cpp
+++ 07_variabel/variabel.cpp
@@ -8,3 +8,17 @@ int main(){
 	std::cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+int main() {
+    int a = 5; // include 5 in the var a which has integer datatype
+    std::cout << a << std::endl; // because datatype of a is integer, so a doesn't need a string.
+    std::cin.get();
+    return 0;
+}
+
+
+
+// ===== 
