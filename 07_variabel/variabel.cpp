c138e599e00400e8f03e1ef77e844a115fc1f720
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-03 16:10:44 +0700

    Development variabel.cpp

--- 07_variabel/variabel.cpp
+++ 07_variabel/variabel.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 
-	int a = 5;
+	int a = 5; // int = integer is bilangan bulat
 
 	std::cout << a << std::endl;
 	std::cin.get();
@@ -21,4 +21,22 @@ int main() {
 
 
 
-// ===== 
+// ===== DEVELOPMENT ===== //
+int main() {
+    int a;
+    int b;
+    int c;
+    std::cout << "welcome to addition operation \n";
+    std::cout << "example : a + b = 1 + 2 = 3" << std::endl;
+    std::cin >> a >> std::endl;
+    std::cin >> b >> std::endl;
+    c = a + b;
+    std::cout << "the addition of ";
+    std::cout << a;
+    std::cout << " + ";
+    std::cout << b;
+    std::cout << " = ";
+    std::cout << c << std::endl;
+    std::cin.get();
+    return 0;
+}
