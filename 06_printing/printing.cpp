e40db7419ef55aacdc4548ea261a84c2b56791dc
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-03 14:09:14 +0700

    Recode printing.cpp

--- 06_printing/printing.cpp
+++ 06_printing/printing.cpp
@@ -8,3 +8,34 @@ int main(){
 	std::cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+
+int main(){
+	std::cout << "halo sodara-sodara sekalian\n";
+	std::cout << "ini adalah baris baru.";
+	std::cout << "ini bukan baris baru.";
+	std::cout << " ini akhir kalimat." << std::endl;
+
+    std::cout << "hi, this is a first line\n"; // \n is for create a new line in the datatype string
+    std::cout << "And this is a second line. ";
+    std::cout << "This is also the second line.";
+    std::cout << "This is a last sentence" << std::endl;
+	
+	std::cin.get();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
