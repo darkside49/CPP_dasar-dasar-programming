2c62d03ee0399ff3ae0f8703a5b8c31f64f31879
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-05 12:53:46 +0700

    Recode tipe_data.cpp

--- 09_tipe data/tipe_data.cpp
+++ 09_tipe data/tipe_data.cpp
@@ -26,3 +26,40 @@ int main(){
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+#include <limits> // must include limits for use numeric_limits function
+int main(){
+	
+    // bilangan bulat
+    int a = 4; // 4 byte / 32 bit
+    long b = 8; // 8 byte / 64 bit
+    short c = 2; // 2 byte / 16 bit
+
+    // bilangan desimal
+    float d = 1.0;
+    double e = 2.5;
+
+    // character
+    char f = "f"; // character 1 bit
+    
+    // boolean
+    bool g = true; // true/false
+	
+    cout << a << endl;
+    cout << sizeof(a) << " byte" << endl; // sizeof is for display/menampilkan size of this datatype.
+    cout << numeric_limits<int>::max() << endl; // numeric_limits max is for display max value of this datatype.
+    cout << numeric_limits<int>::min() << endl; // same of above but the opposite./kebalikannya
+    cin.get();
+    return 0;
+
+}
+
+
+
+
+
+
+
