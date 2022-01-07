d189e83f801e5df2b736f14a6653095321e12a9e
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-07 12:59:34 +0700

    RECODE  komparasi.cpp

--- 11_komparasi/komparasi.cpp
+++ 11_komparasi/komparasi.cpp
@@ -32,3 +32,35 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+
+int main(){
+    
+    int a = 2;
+    int b = 2;
+    
+    bool hasil1, hasil2;
+    
+    // komparasi, relation expression
+	
+    // sebanding / ==
+    hasil1 = (a == b);
+    // tidak sebanding / !=
+    hasil2 = (a != b);
+    
+    // kurang dari
+    hasil1 = (a < b);
+    // lebih dari
+    hasil2 = (a > b);
+    
+    // kurang dari samadengan
+    hasil1 = (a <= b);
+    // lebih dari samadengan
+    hasil2 = (a >= b);
+    
+    cout << hasil1 << endl;
+    cout << hasil2 << endl;
+}
