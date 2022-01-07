fe1e08a93fa14e17de215f61c9c9ec89b4f53d11
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-07 16:02:51 +0700

    Development komparasi.cpp

--- 11_komparasi/komparasi.cpp
+++ 11_komparasi/komparasi.cpp
@@ -64,3 +64,60 @@ int main(){
     cout << hasil1 << endl;
     cout << hasil2 << endl;
 }
+
+
+
+// ===== DEVELOPMENT ===== //
+    
+int bulean(bool hasil) {
+  if (hasil == 1){
+    cout << "jawabannya adalah true / benar" << endl;
+  } else if (hasil == 0){
+    cout << "jawabannya adalah false / salah" << endl;
+  } else {
+    cout << "error";
+  }
+}
+
+int main(){
+    int a = 3;
+    int b = 2;
+    
+    bool hasil, hasil1, hasil2;
+    
+    // komparasi, relation expression
+  
+    // sebanding / ==
+    hasil1 = (a == b);
+    // tidak sebanding / !=
+    hasil2 = (a != b);
+    cout << " apakah " << a << " == " << b << " ? ";
+    hasil = hasil1;
+    bulean(hasil);
+    cout << " apakah " << a << " != " << b << " ? ";
+    hasil = hasil2;
+    bulean(hasil2);
+    
+    // kurang dari
+    hasil1 = (a < b);
+    // lebih dari
+    hasil2 = (a > b);
+    cout << " apakah " << a << "  < " << b << " ? ";
+    hasil = hasil1;
+    bulean(hasil);
+    cout << " apakah " << a << "  > " << b << " ? ";
+    hasil = hasil2;
+    bulean(hasil2);
+    
+    // kurang dari samadengan
+    hasil1 = (a <= b);
+    // lebih dari samadengan
+    hasil2 = (a >= b);
+    
+    cout << " apakah " << a << " <= " << b << " ? ";
+    hasil = hasil1;
+    bulean(hasil);
+    cout << " apakah " << a << " >= " << b << " ? ";
+    hasil = hasil2;
+    bulean(hasil2);
+}
