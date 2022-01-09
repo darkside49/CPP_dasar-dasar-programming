9e641db041b40cd725bfaf053584bd419948c07f
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-09 12:47:20 +0700

    Development logika.cpp

--- 12_logika_digital/logika.cpp
+++ 12_logika_digital/logika.cpp
@@ -78,11 +78,36 @@ int main()
     cout << hasil << endl;
     hasil = (a == 4) || (b == 3); // false && false
     cout << hasil << endl;
+}
 
-	
-	
-	
-	
-	
-	
+
+
+// ===== DEVELOPMENT ===== //
+
+int main() {
+  
+  string username, username_real;
+  bool benar;
+  int pin, pin_real;
+  
+  username_real = username;
+  pin_real = 1234;
+  
+  cout << "SELAMAT DATANG DI ADMINISTRASI JARINGAN \n";
+  cout << "SILAHKAN MASUKKAN USERNAME DAN PIN UNTUK LOGIN \n";
+  cout << "USERNAME : ";
+  cin >> username;
+  cout << "PIN : ";
+  cin >> pin;
+  
+  benar = (username == username_real) and (pin == pin_real);
+  
+  if(benar == 1){
+    cout << "SELAMAT! ANDA BERHASIL LOGIN" << endl;
+  } else if(benar == 0){
+    cout << "username / pin yang anda masukkan salah!" << endl;
+  } else {
+    cout << "udah sholat belum?" << endl;
+  }
+  
 }
