8177d0fc10ce5a1493f9c857a46349b081eba748
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-09 21:36:06 +0700

    Development if.cpp

--- 13_ifstatement/if.cpp
+++ 13_ifstatement/if.cpp
@@ -20,3 +20,44 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+
+int main(){
+  int a;
+  
+  cout << "masukkan angka = ";
+  cin >> a;
+  
+  // if statement
+  // kondisi dalam bentuk boolean
+  if (a == 5) {
+    cout << "halooo" << endl;
+  } else {
+    cout << "hai" << endl;
+  }
+  
+  cout << "selesai" << endl;
+  cin.get();
+  return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+int main(){
+  int angka;
+  
+  cout << "masukkan angka 5 =";
+  cin >> angka;
+  
+  if(angka == 5) {
+    cout << "angka yang anda masukkan benar" << endl;
+  } else {
+    cout << "angka yang anda masukkan salah" << endl;
+  }
+  cin.get();
+  return 0;
+}
