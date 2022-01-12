89f29761d4f75155311a268c23cf9456270225f1
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-12 15:29:11 +0700

    Development switchcase.cpp

--- 15_switch_case/switchcase.cpp
+++ 15_switch_case/switchcase.cpp
@@ -60,4 +60,34 @@ int main(){
   }
   cout << "akhir dari program" << endl;
   return 0;
+}
 
+
+
+// ===== DEVELOPMENT ===== //
+int main(){
+    int pilihan;
+    cout << "pilih menu makanan kesukaanmu:\n";
+    cout << "contoh :\n pizza = 1\n burger = 2\n mie ayam = 3\n sate ayam = 4\n";      
+    cin >> pilihan;
+    
+    switch(pilihan){
+      case 1:
+          cout << "pesanan pizza segera siap" << endl;
+          break;
+      case 2:
+          cout << "pesanan burger segera siap" << endl;
+          break;
+      case 3:
+          cout << "pesanan mie ayam segera siap" << endl;
+          break;
+      case 4:
+          cout << "pesanan sate ayam segera siap" << endl;
+          break;
+      default:
+          cout << "mohon pilih makanan yang ada di daftar menu:)" << endl;   
+    }
+    cout << "akhir dari program" << endl;
+    return 0;
+    
+}
