3adb64f97941ef6c6d95c18b8c7459be4ac954f8
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-15 20:53:52 +0700

    Development dowhile.cpp

--- 20_dowhile/dowhile.cpp
+++ 20_dowhile/dowhile.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
@@ -16,3 +16,36 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+int main(){
+  int a = 1;
+  
+  do{
+    cout << "hore ";
+    cout << a << endl;
+    a++;
+  }while(a <= 10);
+  
+  cout << "selesai" << endl;
+  getch();
+  return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+  int main(){
+    int biner;
+    string he = "he";
+    
+    do{
+      cout << "\n" << he << endl;
+      cout << "apakah kamu ingin tahu perulangan do while? \n";
+      cout << "1 = iya, 0 = tidak, >1 = iya : ";
+      cin >> biner;
+      he += "he";
+    }while(biner == 1 || biner == 0 || biner >= 1);
+  }
