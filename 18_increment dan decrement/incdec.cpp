bc27f6ac39a0ce2995dbd6e730b8a7366853985c
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-15 19:24:32 +0700

    Development incdec.cpp

--- 18_increment dan decrement/incdec.cpp
+++ 18_increment dan decrement/incdec.cpp
@@ -25,3 +25,85 @@ int main()
 	return 0;
 }
 
+
+
+// ===== RECODE ===== //
+int main(){
+  // increment & decrement
+  // preincrement & postincrement
+  // pre = sebelum & post = sesudah
+  // increment = menambah
+  // decrement = mengurangi
+  
+  int a = 10;
+  int b = 10;
+  int c = 10;
+  int d = 10;
+  
+  // postincrement
+  cout << a << endl;
+  cout << a++ << endl;
+  cout << a << endl;
+  
+  // preincrement
+  cout << b << endl;
+  cout << ++b << endl;
+  cout << b << endl;
+  
+  // postdecrement
+  cout << c << endl;
+  cout << c-- << endl;
+  cout << c << endl;
+  
+  // predecrement
+  cout << d << endl;
+  cout << --d << endl;
+  cout << d << endl;
+  
+  getch();
+  return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+int main(){
+  // increment & decrement
+  // preincrement & postincrement
+  // pre = sebelum & post = sesudah
+  // increment = menambah
+  // decrement = mengurangi
+  
+  int a = 10;
+  int b = 10;
+  int c = 10;
+  int d = 10;
+  
+  // postincrement
+  cout << "postincrement" << endl;
+  cout << a << " a" << endl;
+  cout << a++ << " a++" << endl;
+  cout << a << " a\n" << endl;
+  
+  // preincrement
+  cout << "preincrement" << endl;
+  cout << b << " b" << endl;
+  cout << ++b << " ++b" << endl;
+  cout << b << " b\n" << endl;
+  
+  // postdecrement
+  cout << "postdecrement" << endl;
+  cout << c << " c" << endl;
+  cout << c-- << " c--" << endl;
+  cout << c << " c\n" << endl;
+  
+  // predecrement
+  cout << "predecrement" << endl;
+  cout << d << " d" << endl;
+  cout << --d << " --d" << endl;
+  cout << d << " d\n" << endl;
+  
+  getch();
+  return 0;
+}
+
