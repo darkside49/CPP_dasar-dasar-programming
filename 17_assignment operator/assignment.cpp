124be0bdb1dcc3a1aa8d10d76937da8e5abf7974
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-15 18:44:36 +0700

    Recode assignment.cpp

--- 17_assignment operator/assignment.cpp
+++ 17_assignment operator/assignment.cpp
@@ -34,3 +34,38 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+// ==== RECODE ==== //
+int main(){
+    // assignment
+    int a = 10;
+    cout << "nilai awal dari a adalah: " << a << endl;
+    
+    // assignment operator
+    // variabel = variabel operator ekspresi
+    //       a       =        a            -                3;
+    // variabel operator =    ekspresi
+    //       a                +=                3;
+    
+    a += 3;
+    cout << "ditambah 3 menjadi " << a << endl;
+    
+    a -= 3;
+    cout << "dikurangi 3 menjadi " << a << endl;
+    
+    a *= 3;
+    cout << "dikali 3 menjadi " << a << endl;
+    
+    a /= 3;
+    cout << "dibagi 3 menjadi " << a << endl;
+    
+    a%= 3;
+    cout << "dimodulus 3 menjadi " << a <<    endl;
+    getch();
+    return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
