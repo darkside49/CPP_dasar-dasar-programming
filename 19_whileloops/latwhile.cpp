e0f037be9b8c53b35aa530ee1bb3d44741c69c55
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-15 20:39:25 +0700

    Development latwhile.cpp

--- 19_whileloops/latwhile.cpp
+++ 19_whileloops/latwhile.cpp
@@ -18,3 +18,34 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+int main(){
+  int a = 5;
+  while(a <= 10){
+    cout << "a";
+    a += 1;
+  }
+  cout << "selesai" << endl;
+  
+  getch();
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+int main(){
+  int a;
+  
+  cout << "while loop\n";
+  cout << "masukkan nilai awal perulangan mundur (maksimal 10000) : ";     
+  cin >> a;
+  while(a >= 1 && a <= 10000){
+    cout << a << endl;
+    a--;
+  }
+  cout << "program while loop selesai." << endl;
+  getch();
+}
