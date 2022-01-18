ff916c2e639ce8ac81be44f4e586b6fa4f3d07de
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-18 22:49:30 +0700

    Development flow.cpp

--- 22_break dan continue/flow.cpp
+++ 22_break dan continue/flow.cpp
@@ -20,3 +20,58 @@ int main(){
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+int main(){
+  int i = 0;
+  while(i <= 10){
+    i++;
+    if(i == 5){
+      continue;
+      //break;
+    }
+    cout << i << endl;
+  }
+  
+  cout << "akhirnya" << endl;
+  
+  getch();
+  return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+
+#include <string>
+
+int main(){
+  string jawaban;
+  cout << "apakah anda sehat?";
+  cin >> jawaban;
+  if(jawaban == "iya"){
+    string kuat;
+    cout << "apakah anda kuat?";
+    cin >> kuat;
+    if(kuat == "iya"){
+      cout << "anda lolos:)" << endl;
+    } else if(kuat == "tidak"){
+      cout << "anda lemah:(" << endl;
+    } else{
+      cout << "anda harus menjawab antara iya & tidak. selain itu maka anda salah" << endl;      
+    }
+  } else if(jawaban == "tidak"){
+    cout << "anda harus ke rumah sakit" << endl;
+  } else{
+    cout << "anda sanggaatt ppiinnttaarr:)" << endl;
+  }
+  while(true){
+    break;
+  }
+  cout << "selesai" << endl;
+  return 0;
+}
+
+  
