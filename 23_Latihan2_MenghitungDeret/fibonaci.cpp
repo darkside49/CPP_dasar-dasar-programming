78a905a6135dec71b2a27c04ad46dab507ca10e6
Darkside_49 <64604011+darkside49@users.noreply.github.com>
2022-01-26 12:44:17 +0700

    Development fibonaci.cpp

--- 23_Latihan2_MenghitungDeret/fibonaci.cpp
+++ 23_Latihan2_MenghitungDeret/fibonaci.cpp
@@ -30,3 +30,69 @@ int main()
 	cin.get();
 	return 0;
 }
+
+
+
+// ===== RECODE ===== //
+
+
+int main(){
+  // f_n = f_n1 + f_n2
+  
+  int n;
+  int f_n;
+  int f_n1;
+  int f_n2;
+  
+  cout << "Program deret fibonacci \n";
+  cout << "Masukkan nilai ke-n";
+  cin >> n;
+  
+  f_n1 = 1;
+  f_n2 = 0;
+  cout << f_n2 << " ";
+  cout << f_n1 << " ";
+  
+  for(int i = 1; i < n; i++){
+    f_n = f_n1 + f_n2;
+    f_n2 = f_n1;
+    f_n1 = f_n;
+    cout << f_n << " ";
+  }
+  cout << "\n";
+  
+  getch();
+  return 0;
+}
+
+
+
+// ===== DEVELOPMENT ===== //
+int main(){
+  // program fibonacci lebih ringkas
+  // f_n2 = f_n1 + f_n;
+  int n, f_n, f_n1, f_n2;
+  
+  cout << "Program deret fibonacci \n";
+  cout << "Masukkan nilai ke-n";
+  cin >> n;
+  
+  f_n1 = 1;
+  f_n = 0;
+  cout << f_n2 << " ";
+  cout << f_n1 << " ";
+  
+  for(int i = 1; i < n; i++){
+    f_n = f_n1 + f_n2;
+    f_n2 = f_n1;
+    f_n1 = f_n;
+    cout << f_n << " ";
+  }
+  cout << "\n";
+  getch();
+  return 0;
+  
+}
+
+
+
